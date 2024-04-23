d4c46c1d53e255dac8ae3f0217ae5fee327e9ff3
vithumvindeepa <vindeepavithum@gmail.com>
2024-04-23 23:52:53 +0530

    first commit

---
+++ src/main/java/java/lk/ijse/gdse/shoe_shop_management_system/ShoeShopManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package java.lk.ijse.gdse.shoe_shop_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoeShopManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoeShopManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/java/lk/ijse/gdse/shoe_shop_management_system/ShoeShopManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package java.lk.ijse.gdse.shoe_shop_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoeShopManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
