60c1336aed8524ec1c1ce8ea83eeabc949c09900
vithumvindeepa <vindeepavithum@gmail.com>
2024-04-25 11:13:01 +0530

    create interface

--- src/main/java/java/lk/ijse/gdse/shoe_shop_management_system/entity/Sale.java
+++ src/main/java/java/lk/ijse/gdse/shoe_shop_management_system/entity/Sale.java
@@ -1,7 +1,23 @@
 package java.lk.ijse.gdse.shoe_shop_management_system.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 /**
  * @author Vithum vindeepa
  */
+@Data
+@Entity
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "employee")
 public class Sale {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "FIRSTNAME")
+    private String firstName;
 }
--- src/main/java/java/lk/ijse/gdse/shoe_shop_management_system/service/serviceimpl/AdminPanelServiceIpml.java
+++ src/main/java/java/lk/ijse/gdse/shoe_shop_management_system/service/serviceimpl/AdminPanelServiceIpml.java
@@ -3,5 +3,5 @@ package java.lk.ijse.gdse.shoe_shop_management_system.service.serviceimpl;
 /**
  * @author Vithum vindeepa
  */
-public class AdminPanelServiceImpl {
+public class AdminPanelServiceIpml {
 }
