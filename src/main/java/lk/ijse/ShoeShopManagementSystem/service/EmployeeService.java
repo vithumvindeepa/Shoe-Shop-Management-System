26b7cea8879b659961a44f40686d1e76cdde0ee6
vithumvindeepa <vindeepavithum@gmail.com>
2024-05-02 15:19:18 +0530

    create employee serviceImpl

--- src/main/java/lk/ijse/ShoeShopManagementSystem/service/EmployeeService.java
+++ src/main/java/lk/ijse/ShoeShopManagementSystem/service/EmployeeService.java
@@ -1,6 +1,5 @@
 package lk.ijse.ShoeShopManagementSystem.service;
 
-import lk.ijse.ShoeShopManagementSystem.entity.Customer;
 import lk.ijse.ShoeShopManagementSystem.entity.Employee;
 
 import java.util.List;
@@ -10,7 +9,7 @@ import java.util.Optional;
  * @author Vithum vindeepa
  */
 public interface EmployeeService {
-    Customer saveEmployee(Employee employee);
+    Employee saveEmployee(Employee employee);
     List<Employee> getAllEmployee();
     Optional<Employee> findById(String employeeId);
     boolean deleteEmployee(String employeeId);
---
+++ src/main/java/lk/ijse/ShoeShopManagementSystem/serviceimpl/EmployeeServiceImpl.java
@@ -0,0 +1,44 @@
+package lk.ijse.ShoeShopManagementSystem.serviceimpl;
+
+import lk.ijse.ShoeShopManagementSystem.entity.Employee;
+import lk.ijse.ShoeShopManagementSystem.reposirory.EmployeeRepository;
+import lk.ijse.ShoeShopManagementSystem.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author Vithum vindeepa
+ */
+
+@Service
+@RequiredArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    private final EmployeeRepository repository;
+
+    @Override
+    public Employee saveEmployee(Employee employee) {
+        return repository.save(employee);
+    }
+
+    @Override
+    public List<Employee> getAllEmployee() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Optional<Employee> findById(String employeeId) {
+        return repository.findById(employeeId);
+    }
+
+    @Override
+    public boolean deleteEmployee(String employeeId) {
+        repository.deleteById(employeeId);
+        return true;
+    }
+}
