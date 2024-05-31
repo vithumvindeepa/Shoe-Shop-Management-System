52d55aee5ec661a9a12416f0bf02508164cd15c0
vithumvindeepa <vindeepavithum@gmail.com>
2024-05-31 14:10:58 +0530

    Reupload and update suppliers services

--- src/main/java/lk/ijse/ShoeShopManagementSystem/service/SuppliersService.java
+++ src/main/java/lk/ijse/ShoeShopManagementSystem/service/SuppliersService.java
@@ -1,7 +1,18 @@
 package lk.ijse.ShoeShopManagementSystem.service;
 
+import lk.ijse.ShoeShopManagementSystem.entity.Suppliers;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 /**
  * @author Vithum vindeepa
  */
 public interface SuppliersService {
+
+    Supplier saveSupplier(Supplier supplier);
+    List<Supplier> getAllSuppliers();
+    Optional<Supplier> findBySupplierCode(UUID supplierCode);
+    boolean deleteSupplier(Supplier supplierCode);
 }
--- src/main/java/lk/ijse/ShoeShopManagementSystem/serviceimpl/SuppliersServiceImpl.java
+++ src/main/java/lk/ijse/ShoeShopManagementSystem/serviceimpl/SuppliersServiceImpl.java
@@ -1,7 +1,45 @@
 package lk.ijse.ShoeShopManagementSystem.serviceimpl;
 
+
+import lk.ijse.ShoeShopManagementSystem.entity.Suppliers;
+import lk.ijse.ShoeShopManagementSystem.reposirory.SuppliersRepository;
+import lk.ijse.ShoeShopManagementSystem.service.SuppliersService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 /**
  * @author Vithum vindeepa
  */
-public class SuppliersServiceImpl {
+@Service
+@RequiredArgsConstructor
+public class SuppliersServiceImpl implements SuppliersService {
+
+    @Autowired
+    private SuppliersRepository suppliersRepository;
+
+    @Override
+    public Suppliers saveSupplier(Suppliers suppliers) {
+        return suppliersRepository.save(supplier);
+    }
+
+    @Override
+    public List<Suppliers> getAllSuppliers() {
+        return suppliersRepository.findAll();
+    }
+
+    @Override
+    public Optional<Suppliers> findBySupplierCode(UUID supplierCode) {
+        return suppliersRepository.findById(supplierCode);
+    }
+
+    @Override
+    public boolean deleteSupplier(Suppliers supplierCode) {
+        suppliersRepository.delete(supplierCode);
+        return true;
+    }
 }
