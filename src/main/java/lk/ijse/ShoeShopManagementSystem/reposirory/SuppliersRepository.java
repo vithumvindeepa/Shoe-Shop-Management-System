e76cad342b339d5cd1a0c21dc8d1c776d5be6585
vithumvindeepa <vindeepavithum@gmail.com>
2024-05-31 14:07:03 +0530

    Reupload and update suppliiers repo

--- src/main/java/lk/ijse/ShoeShopManagementSystem/reposirory/SuppliersRepository.java
+++ src/main/java/lk/ijse/ShoeShopManagementSystem/reposirory/SuppliersRepository.java
@@ -1,7 +1,12 @@
 package lk.ijse.ShoeShopManagementSystem.reposirory;
 
+import lk.ijse.ShoeShopManagementSystem.entity.Suppliers;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
 /**
  * @author Vithum vindeepa
  */
-public interface SuppliersRepository {
+public interface SuppliersRepository extends JpaRepository<Suppliers, UUID> {
 }
