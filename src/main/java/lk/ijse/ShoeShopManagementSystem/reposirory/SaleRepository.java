83eaa632e23054e6b76c3d57d0a851e2a0960b67
vithumvindeepa <vindeepavithum@gmail.com>
2024-05-31 13:52:36 +0530

    Reupload and update sale repo

--- src/main/java/lk/ijse/ShoeShopManagementSystem/reposirory/SaleRepository.java
+++ src/main/java/lk/ijse/ShoeShopManagementSystem/reposirory/SaleRepository.java
@@ -1,7 +1,16 @@
 package lk.ijse.ShoeShopManagementSystem.reposirory;
 
+import lk.ijse.ShoeShopManagementSystem.entity.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
 /**
  * @author Vithum vindeepa
  */
-public interface SaleRepository {
+public interface SaleRepository extends JpaRepository<Sale, UUID> {
+    Sale findByPurchaseDate(LocalDateTime purchaseDate);
+
+    List<Sale> findByCustomerName(String customerName);
 }
--- src/main/java/lk/ijse/ShoeShopManagementSystem/service/SaleService.java
+++ src/main/java/lk/ijse/ShoeShopManagementSystem/service/SaleService.java
@@ -1,7 +1,22 @@
 package lk.ijse.ShoeShopManagementSystem.service;
 
+import lk.ijse.ShoeShopManagementSystem.dto.SaleDTO;
+import lk.ijse.ShoeShopManagementSystem.entity.Sale;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 /**
  * @author Vithum vindeepa
  */
 public interface SaleService {
+    Sale saveSale(Sale sale);
+    List<Sale> getAllSale();
+    Optional<Sale> findById(UUID saleId);
+    boolean deleteSale(Sale id);
+
+    List<Sale> findByPurchaseDate(LocalDateTime purchaseDate);
+
+    List<Sale> findByCustomerName(String customerName);
 }
