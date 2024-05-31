f87de5d695adc11123c221077d554c558fa713d7
vithumvindeepa <vindeepavithum@gmail.com>
2024-05-31 13:54:02 +0530

    Reupload and update sale DTO

--- src/main/java/lk/ijse/ShoeShopManagementSystem/dto/SaleDTO.java
+++ src/main/java/lk/ijse/ShoeShopManagementSystem/dto/SaleDTO.java
@@ -1,7 +1,32 @@
 package lk.ijse.ShoeShopManagementSystem.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+import java.util.Date;
 /**
  * @author Vithum vindeepa
  */
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class SaleDTO {
+    private String itemCode;
+    private String orderNo;
+    private String customerName;
+    private String itemDesc;
+    private int size;
+    private double unitPrice;
+    private int itemQty;
+    private double totalPrice;
+    private LocalDateTime purchaseDate;
+    private String paymentMethod;
+    private double addedPoints;
+    private String cashierName;
+    private Date createDate;
+    private Date updateDate;
 }
