87cf2eece0646bccb1155c47d7d93e4a05e40848
vithumvindeepa <vindeepavithum@gmail.com>
2024-05-31 14:04:51 +0530

    Reupload and update suppliiersDTO

--- src/main/java/lk/ijse/ShoeShopManagementSystem/dto/SppliiersDTO.java
+++ src/main/java/lk/ijse/ShoeShopManagementSystem/dto/SppliiersDTO.java
@@ -1,7 +1,35 @@
 package lk.ijse.ShoeShopManagementSystem.dto;
 
+import lk.ijse.helloshoeshopmanagement.enums.Category;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Date;
+import java.util.UUID;
+
 /**
- * @author Vithum vindeepa
+ * @author Imalka Gayani
  */
-public class SppliiersDTO {
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class SupplierDTO {
+
+    private UUID supplierCode;
+    private String supplierName;
+    private Category category;
+    private String addressLine1;
+    private String addressLine2;
+    private String addressLine3;
+    private String addressLine4;
+    private String addressLine5;
+    private String addressLine6;
+    private String contactNo1;
+    private String contactNo2;
+    private String email;
+    private Date createDate;
+    private Date updateDate;
 }
