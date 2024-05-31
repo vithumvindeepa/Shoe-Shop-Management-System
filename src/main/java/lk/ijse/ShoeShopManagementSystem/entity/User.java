667c7bcda4a77ff6e4a3334651117e9ea261c6c2
vithumvindeepa <vindeepavithum@gmail.com>
2024-05-31 14:16:28 +0530

    Reupload and update user entity

--- src/main/java/lk/ijse/ShoeShopManagementSystem/entity/User.java
+++ src/main/java/lk/ijse/ShoeShopManagementSystem/entity/User.java
@@ -1,7 +1,38 @@
 package lk.ijse.ShoeShopManagementSystem.entity;
 
+import jakarta.persistence.*;
+import lk.ijse.ShoeShopManagementSystem.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Date;
+import java.util.UUID;
+
 /**
  * @author Vithum vindeepa
  */
+@Entity
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tbl_USER")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "PASSWORD")
+    private String password;
+    @Column(name = "ROLE")
+    private Role role;
+    @Column(name = "CREATE_DATE")
+    private Date createDate;
+    @Column(name = "UPDATE_DATE")
+    private Date updateDate;
 }
